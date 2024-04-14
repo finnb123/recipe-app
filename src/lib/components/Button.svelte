2149a73226d830cdb32213d9db4d6c6e5b68cc6b
brendan <121110218@umail.ucc.ie>
2024-04-14 11:29:17 +0100

    feat: button component

---
+++ src/lib/components/Button.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+    interface Button{
+        title: string;
+        path: string;
+    }
+    export let button:Button;
+
+    const {title, path} = button;
+</script>
+
+
+<div class="flex items-center justify-center">
+    <div class=" w-32 mt-32 p-1 px-2 bg-button border border-stroke rounded-lg text-center">
+    <a href="{path}" class="text-buttonText text-xl"> {title} </a>
+    </div>
+</div>
