9e6ec9dcc4f9ba0e412afcd84a9b85bea9450bfa
brendan <121110218@umail.ucc.ie>
2024-04-15 02:28:05 +0100

    feat: profanity filter function added

---
+++ src/lib/server/filter.ts
@@ -0,0 +1,14 @@
+import {
+    RegExpMatcher,
+    englishDataset,
+    englishRecommendedTransformers
+} from "obscenity";
+
+export const profane = (async (input: string) => {
+    const matcher = new RegExpMatcher({
+        ...englishDataset.build(),
+        ...englishRecommendedTransformers,
+    });
+    const match =  matcher.hasMatch(input);   
+    return match
+})
